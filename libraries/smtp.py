1e96c50f95750348d4afab74d994897ad8f5e10d
Robert Striekwold <rstriekwold@copado.com>
2023-06-12 14:22:05 +0200

    re

--- libraries/smtp.py
+++ libraries/smtp.py
@@ -4,19 +4,11 @@ from robot.api import logger
 
 def send_email(subject, body, sender, recipients, user, password):
     msg = MIMEText(body)
-    logger.warn(body)
     msg['Subject'] = subject
-    logger.warn("subject done")
     msg['From'] = sender
-    logger.warn("sender done")
     msg['To'] = ', '.join(recipients)
-    logger.warn("recipients done")
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    logger.warn("port set")
-    logger.warn("This is the username %s" %(user))
-    logger.warn("This is the apppass %s" %(password))
     smtp_server.login(user, password)
-    logger.warn("login done")
     smtp_server.sendmail(sender, recipients, msg.as_string())
     logger.warn("mail send")
     smtp_server.quit()
