4648afe7d897509ff169055bcf8b57b3ef9134cf
Robert Striekwold <rstriekwold@copado.com>
2023-06-12 14:23:41 +0200

    re

--- libraries/smtp.py
+++ libraries/smtp.py
@@ -6,7 +6,7 @@ def send_email(subject, body, sender, recipients, user, password):
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
+    msg['To'] = recipients
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     smtp_server.login(user, password)
     smtp_server.sendmail(sender, recipients, msg.as_string())
