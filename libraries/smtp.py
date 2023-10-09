5f7e9faf65140fad4d0cccf4fdf5ef426e6514ed
Robert Striekwold <rstriekwold@copado.com>
2023-10-09 08:58:35 +0200

    test

--- libraries/smtp.py
+++ libraries/smtp.py
@@ -17,7 +17,8 @@ def reply_email(subject, body, sender, recipients, user, password):
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['Reply-To'] = recipients
+    msg['To'] = recipients
+    msg.add_header('reply-to', recipients)
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     smtp_server.login(user, password)
     smtp_server.sendmail(sender, recipients, msg.as_string())
